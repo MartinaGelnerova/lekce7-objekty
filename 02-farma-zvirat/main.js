b10a6621eb541816b00b0fdfd5590a9cfed94616
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-03 13:18:04 +0200

    upraveno pro více zvířat

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -1,43 +1,36 @@
-let krava = {
-    jmeno: 'Kravička',
-    foto: 'obrazky/krava.jpg'
-};
+let zvirata = [
+  {zvire: 'husa', jmeno: 'Husička'},
+  {zvire: 'kocka', jmeno: 'Kočička'}, 
+  {zvire: 'krava', jmeno: 'Kravička'},
+  {zvire: 'kun', jmeno: 'Koníček'},
+  {zvire: 'ovce', jmeno: 'Ovečka'},
+  {zvire: 'pes', jmeno: 'Pejsek'}
+];
 
-let ovecka = {
-    jmeno: 'Ovečka',
-    foto: 'obrazky/ovce.jpg'
-};
+// přiřadíme zvířatům fotky
+zvirata.forEach(pridejObrazek)
+function pridejObrazek(zvire) {
+	zvire.foto = `obrazky/${zvire.zvire}.jpg`;
+	return zvire.foto;
+}
 
-// Vytvor obrazek zviratka krava s fotkou a popiskem.
+// zobrazíme info do webu
+zvirata.forEach(naWeb)
+function naWeb(zvire) {
+  let zvireWeb = document.createElement('div');
+  zvireWeb.className = 'zvire';
+  let fotoZvirete = document.createElement('img');
+  fotoZvirete.className = 'foto';
+  fotoZvirete.src = zvire.foto;
+  let jmenoZvirete = document.createElement('span');
+  jmenoZvirete.className = 'jmeno';
+  jmenoZvirete.innerHTML = zvire.jmeno;
+  zvireWeb.appendChild(fotoZvirete);
+  zvireWeb.appendChild(jmenoZvirete);    
+  let farma = document.querySelector('.farma');
+  farma.appendChild(zvireWeb);
+}
 
-// 1. Zacneme vytvorenim obalujiciho div element s tridou 'zvire'.
-
-let zvire = document.createElement('div');
-zvire.className = 'zvire';
-
-// 2. Nyni vytvor dva elementy pro obrazek a jmeno zvirete, ktere budou uvnitr naseho obalujiciho div elementu zvire.
-    //2.1. Vytvor img element s tridou 'foto'. Jako src obrazku pridej hodnotu objektu krava.
-
-let fotoZvirete = document.createElement('img');
-fotoZvirete.className = 'foto';
-fotoZvirete.src = krava.foto;
-
-    //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
-
-let jmenoZvirete = document.createElement('span');
-jmenoZvirete.className = 'jmeno';
-jmenoZvirete.innerHTML = krava.jmeno;
-
-    //2.3. Zabal fotoZvire a jmenoZvirete do obalovaciho divu zvire, (psst zvire.appendChild)
-
-zvire.appendChild(fotoZvirete);
-zvire.appendChild(jmenoZvirete);
-
-// 3. Nakonec pojdme nas kod zapojit do html stranky. Na html strance vyber existujici element s tridou farma.
-// a vloz do nej obalujici element zvire (pssst appendChild)
-
-let farma = document.querySelector('.farma');
-farma.appendChild(zvire);
 
 
 // Bonus
